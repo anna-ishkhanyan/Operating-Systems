3d722ce8a4d16af06d9d725bea876a2dee9d94c9
anna-ishkhanyan <127134473+anna-ishkhanyan@users.noreply.github.com>
2025-09-14 23:30:00 +0400

    Update Assignment2.c

--- Fork & Exec programs/Assignment2.c
+++ Fork & Exec programs/Assignment2.c
@@ -5,36 +5,36 @@
 #include <stdlib.h>
 
 int main(){
-        pid_t pid1 = fork(); // creates the first child process
+	pid_t pid1 = fork(); // creates the first child process
 	if (pid1 < 0){ // fails to create
                 perror("fork");
-		exit(EXIT_FAILURE);
+	        	exit(EXIT_FAILURE);
 	}
-        else if (pid1 == 0){ // child process
-                int ret; 
-                ret = execl("/bin/ls", "ls", ".",NULL);
-                if (ret == -1){ 
-                        perror ("execl");
-		        exit(EXIT_FAILURE);
+    else if (pid1 == 0){ // child process
+		int ret; 
+        ret = execl("/bin/ls", "ls", ".",NULL);
+        if (ret == -1){
+			perror ("execl");
+		    exit(EXIT_FAILURE);
 		}
-
-        }
+    }
         
 	pid_t pid2 = fork(); // creates the second child process
-        if (pid2 < 0){ // fails to create
-                perror("fork");
-	        exit(EXIT_FAILURE);
+    if (pid2 < 0){ // fails to create
+		perror("fork");
+		exit(EXIT_FAILURE);
 	}
-        else if (pid2 == 0){ // child process
-                int ret; 
-                ret = execl("/bin/date", "date",NULL);
-                if (ret == -1){
-                        perror ("execl");
-		        exit(EXIT_FAILURE);
+    else if (pid2 == 0){ // child process
+		int ret; 
+        ret = execl("/bin/date", "date",NULL);
+		if (ret == -1){
+			perror ("execl");
+			exit(EXIT_FAILURE);
 		}
-        }
+    }
 	wait(NULL);
 	wait(NULL); // as we have two child processes
-        printf("Parent process done\n");
-        return 0;
+    printf("Parent process done\n");
+    
+	return 0;
 }
